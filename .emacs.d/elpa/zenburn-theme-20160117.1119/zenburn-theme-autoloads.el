e88e4bf43b99e0ad720471215b86fa20ce864a10
Klaus <derkoenig@gmail.com>
2016-10-16 23:15:40 -0400

    Update config files. Remove package folders.

--- .emacs.d/elpa/zenburn-theme-20160117.1119/zenburn-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; zenburn-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (22194 17744
-;;;;;;  560036 450000))
-;;; Generated autoloads from zenburn-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("zenburn-theme-pkg.el") (22194 17744 583344
-;;;;;;  927000))
-
-;;;***
-
-(provide 'zenburn-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; zenburn-theme-autoloads.el ends here
