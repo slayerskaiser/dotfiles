e84f60181181661c84f69a7f0b4abd0b79f71ebc
Klaus <derkoenig@gmail.com>
2018-11-06 12:01:34 -0600

    Add config for xmobar

--- xmonad.hs
+++ xmonad.hs
@@ -1,9 +1,32 @@
 import XMonad
 import XMonad.Actions.UpdatePointer
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.DynamicLog
 
-main = xmonad defaultConfig
-	{ modMask = mod3Mask
-	, terminal = "lxterminal"
-	, logHook = updatePointer (Relative 0.5 0.5)
+main = xmonad =<< statusBar myBar myPP toggleStrutKey myConfig
+
+myBar = "xmobar"
+
+-- myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+myPP = xmobarPP
+    { ppCurrent = xmobarColor "yellow" "" . wrap "[" "]"
+    , ppVisible = wrap "(" ")"
+    , ppTitle = xmobarColor "cyan" "" . shorten 100
+    }
+
+toggleStrutKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+myConfig = defaultConfig
+    { modMask = mod1Mask
+	, terminal = "xfce4-terminal"
+	, logHook = updatePointer (0.5,0.5) (0,0)
+    , layoutHook = smartBorders $ layoutHook defaultConfig
 	}
+
+-- main = xmonad defaultConfig
+-- 	{ modMask = mod1Mask
+-- 	, terminal = "xfce4-terminal"
+-- 	, logHook = updatePointer (0.5,0.5) (0,0)
+--     , layoutHook = smartBorders $ layoutHook defaultConfig
+-- 	}
 
