d44ba8b17824fb0c28d0e30ce769f178f9e1f3e4
Klaus <derkoenig@gmail.com>
2018-11-08 22:26:38 -0600

    Change xmonad layouts and xmobar display

--- xmonad.hs
+++ xmonad.hs
@@ -2,12 +2,17 @@ import XMonad
 import XMonad.Actions.UpdatePointer
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.DynamicLog
+-- import qualified Data.Map as M
+-- import qualified XMonad.Stackset as W
+import XMonad.Util.EZConfig
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
 
 main = xmonad =<< statusBar myBar myPP toggleStrutKey myConfig
 
 myBar = "xmobar"
 
--- myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
 myPP = xmobarPP
     { ppCurrent = xmobarColor "yellow" "" . wrap "[" "]"
     , ppVisible = wrap "(" ")"
@@ -16,17 +21,35 @@ myPP = xmobarPP
 
 toggleStrutKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
+myLayout = mkToggle (single MIRROR)
+    . mkToggle (single FULL)
+    $ tall ||| ThreeColMid nmaster delta ratio
+    where
+        tall = Tall nmaster delta ratio
+        nmaster = 1
+        delta = 3/100
+        ratio = 1/2
+
+-- myKeys (XConfig {XMonad.modMask = modMask}) = M.fromList $
+--     [ ((modMask .|. controlMask, xK_m), spawn "amixer -q set Master toggle")
+--     , ((modMask .|. controlMask, xK_j), spawn "amixer -q set Master 5%-")
+--     , ((modMask .|. controlMask, xK_k), spawn "amixer -q set Master 5%+")
+--     , ((modMask .|. controlMask, xK_l), spawn "xlock -mode blank")
+--     ]
+
+myKeys =
+    [ ("M-C-m", spawn "amixer -q set Master toggle")
+    , ("M-C-j", spawn "amixer -q set Master 5%-")
+    , ("M-C-k", spawn "amixer -q set Master 5%+")
+    , ("M-C-l", spawn "xlock -mode blank")
+    , ("M-f", sendMessage $ Toggle FULL)
+    , ("M-C-<Space>", sendMessage $ Toggle MIRROR)
+    ]
+
 myConfig = defaultConfig
     { modMask = mod1Mask
 	, terminal = "xfce4-terminal"
 	, logHook = updatePointer (0.5,0.5) (0,0)
-    , layoutHook = smartBorders $ layoutHook defaultConfig
-	}
-
--- main = xmonad defaultConfig
--- 	{ modMask = mod1Mask
--- 	, terminal = "xfce4-terminal"
--- 	, logHook = updatePointer (0.5,0.5) (0,0)
---     , layoutHook = smartBorders $ layoutHook defaultConfig
--- 	}
-
+    , layoutHook = smartBorders $ myLayout -- $ layoutHook defaultConfig
+--    , keys = \c -> myKeys c `M.union` keys defaultConfig c
+	} `additionalKeysP` myKeys
