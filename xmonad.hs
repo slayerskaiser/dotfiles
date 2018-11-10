1fb3bcc13b1173ad9f138f01fc1e76717dcb6dab
Klaus <derkoenig@gmail.com>
2018-11-10 12:38:21 -0600

    Change xmonad keys
    Change volume keys to free M-C-k for typeracer
    Move M-{w,e,r} to M-{a,s,d} to free M-w for copying

--- xmonad.hs
+++ xmonad.hs
@@ -3,9 +3,9 @@ import XMonad.Actions.UpdatePointer
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.DynamicLog
 -- import qualified Data.Map as M
--- import qualified XMonad.Stackset as W
+import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
-import XMonad.Layout.ThreeColumns
+import XMonad.Layout.TwoPane
 import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
 
@@ -23,7 +23,7 @@ toggleStrutKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 myLayout = mkToggle (single MIRROR)
     . mkToggle (single FULL)
-    $ tall ||| ThreeColMid nmaster delta ratio
+    $ tall ||| TwoPane delta ratio
     where
         tall = Tall nmaster delta ratio
         nmaster = 1
@@ -38,12 +38,17 @@ myLayout = mkToggle (single MIRROR)
 --     ]
 
 myKeys =
-    [ ("M-C-m", spawn "amixer -q set Master toggle")
-    , ("M-C-j", spawn "amixer -q set Master 5%-")
-    , ("M-C-k", spawn "amixer -q set Master 5%+")
+    [ ("M-S-m", spawn "amixer -q set Master toggle")
+    , ("M-S-j", spawn "amixer -q set Master 5%-")
+    , ("M-S-k", spawn "amixer -q set Master 5%+")
     , ("M-C-l", spawn "xlock -mode blank")
     , ("M-f", sendMessage $ Toggle FULL)
     , ("M-C-<Space>", sendMessage $ Toggle MIRROR)
+    ]
+    ++
+    [ (mask ++ "M-" ++ [key], screenWorkspace scr >>= flip whenJust (windows . action))
+        | (key, scr) <- zip "asd" [0..]
+        , (action, mask) <- [ (W.view, "") , (W.shift, "S-") ]
     ]
 
 myConfig = defaultConfig
@@ -52,4 +57,4 @@ myConfig = defaultConfig
 	, logHook = updatePointer (0.5,0.5) (0,0)
     , layoutHook = smartBorders $ myLayout -- $ layoutHook defaultConfig
 --    , keys = \c -> myKeys c `M.union` keys defaultConfig c
-	} `additionalKeysP` myKeys
+	} `removeKeysP` [ ("M-w"), ("M-e"), ("M-r") ] `additionalKeysP` myKeys
